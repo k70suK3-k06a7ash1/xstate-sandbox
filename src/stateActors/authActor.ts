a4360ac945f7cab77c11da869b51934c66dce7a8
2814109 <2814109k@gmail.com>
2024-05-20 01:34:42 +0900

    add toglleMachine

--- src/stateActors/authActor.ts
+++ src/stateActors/authActor.ts
@@ -1,20 +1,28 @@
-import { fromPromise, setup } from "xstate";
+// import { fromPromise, setup } from "xstate";
 
-interface Input {
-  readonly source: string;
-}
-const authActor = setup({
-  types: {
-    input: {} as Input,
-  },
-  actions: {
-    auth: () =>
-      fromPromise(() => {
-        return fetch("https://syntax.fm/api/shows/latest").then((data) =>
-          data.json()
-        );
-      }),
-  },
-}).createMachine({
-  id: "auth-actor",
-});
+// interface Input {
+//   readonly source: string;
+// }
+// export const authActor = setup({
+//   types: {
+//     input: {} as Input,
+//   },
+//   actions: {
+//     auth: () =>
+//       fromPromise(() => {
+//         return fetch("https://syntax.fm/api/shows/latest").then((data) =>
+//           data.json()
+//         );
+//       }),
+//   },
+// }).createMachine({
+//     id: "auth-actor",
+//     invoke: {
+//         src: "onInit",
+//         input: ({ event }) => {
+//             if (event.type === "xstate.init") {
+//                 return event.input
+//             }
+//         }
+//     }
+// });
---
+++ src/stateActors/toggleActor.ts
@@ -0,0 +1,29 @@
+/**
+ * Creates a new Interpreter instance for the given machine with the provided options, if any.
+ *
+ * @description Use `createActor` instead
+ */
+import { createMachine, createActor } from "xstate";
+
+// State machine definition
+const toggleMachine = createMachine({
+  id: "toggle",
+  initial: "inactive",
+  states: {
+    inactive: { on: { TOGGLE: "active" } },
+    active: { on: { TOGGLE: "inactive" } },
+  },
+});
+
+// Machine instance with internal state
+const toggleActor = createActor(toggleMachine);
+toggleActor.subscribe((state) => {
+  console.log(state.value);
+});
+toggleActor.start();
+// => logs 'inactive'
+
+toggleActor.send({ type: "TOGGLE" });
+// => logs 'active'
+
+toggleActor.send({ type: "TOGGLE" });
