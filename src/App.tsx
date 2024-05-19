ef05cf8cc23e456875d25324eeaef4dc47b1f66a
2814109 <2814109k@gmail.com>
2024-05-19 21:16:47 +0900

    refactor: update App component with JSON output and audio controls.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ function App() {
   return (
     <>
       <div>
-        {JSON.stringify(snapshot)}
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +28,35 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <button onClick={() => send({ type: "play" })}>play</button>
+      <pre>{JSON.stringify(snapshot.value, null, 2)}</pre>
+      <audio
+        crossOrigin="anonymous"
+        src="https://audio.transistor.fm/m/shows/40155/2658917e74139f25a86a88d346d71324.mp3"
+        onTimeUpdate={({ currentTarget: audioRef }) =>
+          send({ type: "time", params: { updatedTime: audioRef.currentTime } })
+        }
+        onError={({ type }) =>
+          send({ type: "init-error", params: { message: type } })
+        }
+        onLoadedData={({ currentTarget: audioRef }) =>
+          send({ type: "loading", params: { audioRef } })
+        }
+        onEnded={() => send({ type: "end" })}
+      />
+      <p>{`Current time: ${snapshot.context.currentTime}`}</p>
+      <div>
+        {snapshot.matches({ Active: "Paused" }) && (
+          <button onClick={() => send({ type: "play" })}>Play</button>
+        )}
+
+        {snapshot.matches({ Active: "Playing" }) && (
+          <button onClick={() => send({ type: "pause" })}>Pause</button>
+        )}
+
+        {snapshot.matches("Active") && (
+          <button onClick={() => send({ type: "restart" })}>Restart</button>
+        )}
+      </div>{" "}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
